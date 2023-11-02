b0f47c3f3f52cf7fd22b714505deec1733819e58
ambraxoxo <ambram98@gmail.com>
2023-11-02 17:35:33 +0100

    add utils and socket

--- farm.c
+++ farm.c
@@ -8,8 +8,18 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include "conn.h"
+#include "util.h"
+
+#define SOCKNAME "farm.sck"
+#define UNIX_PATH_MAX 108
 
 BQueue_t* taskqueue;
+struct sockaddr_un sa;
+
 //socket per comunicazione con Collector, passo il socket ai thread? oppure struttura con riferimento alla coda e alla socket 
 
 /*typedef struct threadArgs{
@@ -17,31 +27,40 @@ BQueue_t* taskqueue;
     //socket?    
 }threadArgs_t;*/
 
-typedef struct fileType{
+/*typedef struct fileType{
     char* name;
     size_t size;
-}fileType_t;
+}fileType_t;*/
 
 typedef struct resType{
     char* filename;
     long result;
 } resType_t;
 
+void cleanup() {
+    unlink(SOCKNAME);
+}
+
 static void *worker(void* args){
     fflush(stdout);
+    struct stat statbuf;
+    size_t size;
     long res;
     int n_long;
     //BQueue_t *q = ((threadArgs_t*)args)->q;
     
-    fileType_t* el = pop(taskqueue);
+    char* el = pop(taskqueue);
+    if(stat(el, &statbuf) == 0){
+        size = statbuf.st_size;
+    }
     if(el == NULL){
         printf("pop\n");
         exit(EXIT_FAILURE);
     }
     FILE* file; 
-    n_long = (el->size)/8;
+    n_long = size/8;
     long buff[n_long];
-    if((file = fopen(el->name, "r")) == NULL){
+    if((file = fopen(el, "r")) == NULL){
         perror("fopen");
         exit(EXIT_FAILURE);
     }
@@ -56,21 +75,22 @@ static void *worker(void* args){
         res += i*buff[i];
     }
     resType_t r;
-    r.filename = el->name;
+    r.filename = el;
     r.result = res;
     printf("%ld\n", res);
 }
 
-int isRegular(const char filename[], size_t* size){
+void processdir(char* dirpath){
+    DIR* d;
+    struct dirent* file;
+}
+
+int isRegular(const char filename[]){
     struct stat statbuf;
-    size_t fsize;
 
     if(stat(filename, &statbuf) == 0){
         if(S_ISREG(statbuf.st_mode)){    
-            if(size != NULL){ 
-                *size = statbuf.st_size;
-                return 1;
-            }
+            return 1;
         }
     }
     return 0;
@@ -88,6 +108,7 @@ int main(int argc, char *argv[]){
         printf("Uso: %s [file list]", argv[0]);
         return -1;
     }
+    strncpy(sa.sun_path, SOCKNAME, UNIX_PATH_MAX);
     int nThreads = 4;
     int qLen = 8;
     int delay = 0;
@@ -133,7 +154,7 @@ int main(int argc, char *argv[]){
         }
 
         for(int i = optind; i < argc; i++){
-            if(argv[i][1] == 'd'){
+            /*if(argv[i][1] == 'd'){
                 if(isDir(&argv[++i])){
                     DIR * d;
                     struct dirent* ent;
@@ -145,11 +166,9 @@ int main(int argc, char *argv[]){
                         
                     }
                 }
-            }
-            fileType_t* file = malloc(sizeof(fileType_t));  
-            if(isRegular(argv[i], &file->size)){
-                file->name = argv[i];
-                if(push(taskqueue, file) == -1){
+            }*/
+            if(isRegular(argv[i])){
+                if(push(taskqueue, argv[i]) == -1){
                     perror("push");
                     exit(EXIT_FAILURE);
                 }
@@ -160,6 +179,28 @@ int main(int argc, char *argv[]){
         }
         for (int i = 0; i < nThreads; i++) {
             pthread_join(workers[i], NULL);
+        }
+    }else{
+        /*----------COLLECTOR----------*/
+        int fd_skt, fd_c, notused;
+        if((fd_skt = socket(AF_UNIX, SOCK_STREAM, 0)) == -1 ){
+            perror("creazione socket");
+            exit(EXIT_FAILURE);
+        }
+
+        if((notused = bind(fd_skt, (struct sockaddr *)&sa, sizeof(sa))) == -1){
+            perror("bind");
+            exit(EXIT_FAILURE);
+        }
+
+        if((notused = listen(fd_skt, SOMAXCONN)) == -1){
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+
+        if((fd_c = accept(fd_skt, NULL, 0)) == -1){
+            perror("accept");
+            exit(EXIT_FAILURE);
         }
     }
 
